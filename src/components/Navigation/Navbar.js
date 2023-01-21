751e0ad0ef35560be6dfa7e354f00fadbf5f7122
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-21 21:49:41 +0100

    navigation - Navbar

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+
+const Navbar = () => {
+	return (
+		<Wrapper>
+			<div className="nav-center">
+				<button
+					type="button"
+					className="toggle-btn"
+					onClick={() => {
+						console.log("====================================");
+						console.log("BTN");
+						console.log("====================================");
+					}}
+				>
+					<FaAlignLeft />
+				</button>
+				<div>
+					<h2>TAG</h2>
+					<h3 className="logo-text">Solution</h3>
+				</div>
+			</div>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.nav`
+	height: 4rem;
+	background: var(--ColorWhite);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	box-shadow: 0 1px 0px 0px rgba(0, 0, 0, 0.1);
+	.nav-center {
+		display: flex;
+		width: 90vw;
+		align-items: center;
+		justify-content: space-between;
+		.toggle-btn {
+			background: transparent;
+			border-color: transparent;
+			font-size: 1.75rem;
+			color: var(--ColorCrimson);
+			cursor: pointer;
+			display: flex;
+			align-items: center;
+		}
+		h2 {
+			text-align: center;
+			font-size: 1.7rem;
+			letter-spacing: 0.2rem;
+			color: var(--ColorCrimson);
+		}
+		.logo-text {
+			display: none;
+			margin: 0;
+			font-size: 1rem;
+			letter-spacing: 0.07rem;
+			color: var(--ColorCrimson);
+		}
+	}
+
+	@media (min-width: 992px) {
+		position: sticky;
+		top: 0;
+		.nav-center {
+			width: 90%;
+			h2 {
+				font-size: 1.3rem;
+			}
+			.logo-text {
+				display: block;
+			}
+		}
+	}
+`;
+
+export default Navbar;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,4 @@
+import Navbar from "./Navbar";
+import SidebarSmall from "./SidebarSmall";
+import Sidebar from "./Sidebar";
+export { Navbar, SidebarSmall, Sidebar };
